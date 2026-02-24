6179ea0d3a93aa6d03d29d081fe899ea562f081e
Narf-stack <fran.stacksta@gmail.com>
2026-02-24 10:31:48 +0100

    Save the entire pipeline

--- ml/train.py
+++ ml/train.py
@@ -5,11 +5,17 @@
 from training import *
 
 
-df = load_data("../data/telco_customer_churn.csv")
+if __name__ == "__main__":
+  df = load_data("../data/telco_customer_churn.csv")
 
-X_train, X_val, X_test, y_train, y_val, y_test = split_data(df)
+  X_train, X_val, X_test, y_train, y_val, y_test = split_data(df)
 
-numerical_features, categorical_features = get_feature_types(X_train)
-preprocessor = build_preprocessor(numerical_features, categorical_features)
-pipeline = build_pipeline(preprocessor)
-pipeline.fit(X_train, y_train)
+  numerical, categorical = get_feature_types(X_train)
+
+  preprocessor = build_preprocessor(numerical, categorical)
+
+  pipeline = build_pipeline(preprocessor)
+
+  pipeline.fit(X_train, y_train)
+
+  save_model(pipeline)
--- ml/training/__init__.py
+++ ml/training/__init__.py
@@ -3,6 +3,7 @@ from splitter import split_data
 from feature_types import get_feature_types
 from build_preprocessor import build_preprocessor
 from build_pipeline import build_pipeline
+from model_saver import save_model
 
 
-__all__ = ["load_data", "split_data", "get_feature_types", "build_preprocessor", "build_pipeline"]
+__all__ = ["load_data", "split_data", "get_feature_types", "build_preprocessor", "build_pipeline", "save_model"]
