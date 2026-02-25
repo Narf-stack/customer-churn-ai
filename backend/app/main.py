f063111ffbe940bcd88809db5f4646d8be58fde6
Narf-stack <fran.stacksta@gmail.com>
2026-02-25 13:15:47 +0100

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
-from .api.v1.endpoints import predict
+from contextlib import asynccontextmanager
+import json
+from pathlib import Path
+from backend.app.core import registry
+from backend.app.api.v1.endpoints import predict
+from backend.app.ml import model, preprocessor, threshold
 
-app = FastAPI(title="Customer Churn API")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  registry.model = model
+  registry.preprocessor = preprocessor
+  registry.threshold = threshold
+
+  print("Model artifacts loaded successfully.")
+
+  yield
+
+app = FastAPI(
+  title="Customer Churn API",
+  lifespan=lifespan
+)
 
 app.include_router(predict.router, prefix="/api/v1")
